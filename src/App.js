12a489d591fab120c01989827b3cac893f2a2bec
Cristian Julio <=>
2020-10-29 15:47:30 -0500

    App terminada

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Clima from "./components/Clima";
+import Error from "./components/Error";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
-function App() {
+const App = () => {
+  const [busqueda, setBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const [consultar, setConsultar] = useState(false);
+  const [resultado, setResultado] = useState({});
+  const [error, setError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      const appid = "b72c6947bbe6a741832980d5337d6318";
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appid}`;
+
+      const consultarApi = await fetch(url);
+      const datos = await consultarApi.json();
+
+      setResultado({ ...datos });
+      setConsultar(false);
+
+      if (datos.cod === "404") {
+        setError(true);
+      } else {
+        setError(false);
+      }
+    };
+
+    if (consultar) consultarApi();
+    // eslint-disable-next-line
+  }, [consultar]);
+
+  let componente;
+
+  if (error) {
+    componente = <Error mensaje="No hay resultados" />;
+  } else {
+    componente = <Clima resultado={resultado} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header titulo="Clima React App" />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
+            </div>
+            <div className="col m6 s12">{componente}</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
